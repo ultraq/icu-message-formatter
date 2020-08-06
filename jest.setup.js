55a7fe961803a6f654eacd46c03952efb20d3acf
Emanuel Rabina <emanuelrabina@gmail.com>
2020-08-06 16:55:13 +1200

    Lint!

--- jest.setup.js
+++ jest.setup.js
@@ -1,5 +1,5 @@
 /* eslint-env node */
-'use strict';
+'use strict'; // eslint-disable-line
 
 // Node has an Intl object, but doesn't ship with any locale information, so we
 // need to patch parts of it to work as it does in the browser.

3199804256c62cdecd54c4f9e4a1b1f02f04661c
Emanuel Rabina <emanuelrabina@hey.com>
2021-02-09 21:39:37 +1300

    Use a more recently-maintained intl library

--- jest.setup.js
+++ jest.setup.js
@@ -3,11 +3,5 @@
 
 // Node has an Intl object, but doesn't ship with any locale information, so we
 // need to patch parts of it to work as it does in the browser.
-const IntlPolyfill = require('intl');
-Intl.NumberFormat = IntlPolyfill.NumberFormat;
-IntlPolyfill.__applyLocaleSensitivePrototypes();
-// Object.defineProperty(Number.prototype, 'toLocaleString', {
-// 	configurable: true,
-// 	writable: true,
-// 	value: IntlPolyfill.__localeSensitiveProtos.Number.toLocaleString
-// });
+require('@formatjs/intl-numberformat/polyfill');
+require('@formatjs/intl-numberformat/locale-data/en-NZ');

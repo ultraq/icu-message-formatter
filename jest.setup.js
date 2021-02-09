a7991f04da2a27f2310b4ba597ecde921cc023b6
Emanuel Rabina <emanuelrabina@hey.com>
2021-02-09 21:47:15 +1300

    intl-locale polyfill also needed for Node 10

--- jest.setup.js
+++ jest.setup.js
@@ -3,5 +3,6 @@
 
 // Node has an Intl object, but doesn't ship with any locale information, so we
 // need to patch parts of it to work as it does in the browser.
+require('@formatjs/intl-locale/polyfill');
 require('@formatjs/intl-numberformat/polyfill');
 require('@formatjs/intl-numberformat/locale-data/en-NZ');

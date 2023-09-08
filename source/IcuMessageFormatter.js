578b7f8227c39dd4f6128ed8d4d0e0193304df24
Emanuel Rabina <emanuelrabina@gmail.com>
2023-09-08 23:05:08 +1200

    Use updated ESLint config to fix JSDocs

--- source/IcuMessageFormatter.js
+++ source/IcuMessageFormatter.js
@@ -17,8 +17,4 @@
 export {default as MessageFormatter}  from './MessageFormatter.js';
 export {default as pluralTypeHandler} from './pluralTypeHandler.js';
 export {default as selectTypeHandler} from './selectTypeHandler.js';
-export {
-	findClosingBracket,
-	parseCases,
-	splitFormattedArgument
-}                                     from './utilities.js';
+export {findClosingBracket, parseCases, splitFormattedArgument} from './utilities.js';

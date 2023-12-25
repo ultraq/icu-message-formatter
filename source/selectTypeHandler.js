2b16384d41bf0fa792bed697a7b555dec71aa422
Emanuel Rabina <emanuelrabina@gmail.com>
2023-12-26 09:26:50 +1300

    Use import to fix types for other exports

--- source/selectTypeHandler.js
+++ source/selectTypeHandler.js
@@ -1,12 +1,12 @@
-/* 
+/*
  * Copyright 2019, Emanuel Rabina (http://www.ultraq.net.nz/)
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -21,18 +21,18 @@ const OTHER = 'other';
 /**
  * Handler for `select` statements within ICU message syntax strings.  Returns
  * a formatted string for the branch that closely matches the current value.
- * 
+ *
  * See https://formatjs.io/docs/core-concepts/icu-syntax#select-format for more
  * details on how the `select` statement works.
- * 
+ *
  * @param {string} value
  * @param {string} matches
  * @param {string} locale
- * @param {Record<string,any>} values
- * @param {(message: string, values?: Record<string,any>) => any[]} process
+ * @param {import('./MessageFormatter.js').FormatValues} values
+ * @param {import('./MessageFormatter.js').ProcessFunction} process
  * @return {any | any[]}
  */
-export default function selectTypeHandler(value, matches = '', locale, values, process) {
+export default function selectTypeHandler(value, matches, locale, values, process) {
 	const {cases} = parseCases(matches);
 
 	if (value in cases) {
--- source/utilities.js
+++ source/utilities.js
@@ -1,12 +1,12 @@
-/* 
+/*
  * Copyright 2019, Emanuel Rabina (http://www.ultraq.net.nz/)
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -26,16 +26,16 @@
  * Most branch-based type handlers are based around "cases".  For example,
  * `select` and `plural` compare compare a value to "case keys" to choose a
  * subtranslation.
- * 
+ *
  * This util splits "matches" portions provided to the aforementioned handlers
  * into case strings, and extracts any prepended arguments (for example,
  * `plural` supports an `offset:n` argument used for populating the magic `#`
  * variable).
- * 
+ *
  * @param {string} string
  * @return {ParseCasesResult}
  */
-export function parseCases(string) {
+export function parseCases(string = '') {
 	const isWhitespace = ch => /\s/.test(ch);
 
 	const args = [];
@@ -106,7 +106,7 @@ export function parseCases(string) {
 /**
  * Finds the index of the matching closing curly bracket, including through
  * strings that could have nested brackets.
- * 
+ *
  * @param {string} string
  * @param {number} fromIndex
  * @return {number}
@@ -133,7 +133,7 @@ export function findClosingBracket(string, fromIndex) {
 /**
  * Split a `{key, type, format}` block into those 3 parts, taking into account
  * nested message syntax that can exist in the `format` part.
- * 
+ *
  * @param {string} block
  * @return {string[]}
  *   An array with `key`, `type`, and `format` items in that order, if present
@@ -146,7 +146,7 @@ export function splitFormattedArgument(block) {
 /**
  * Like `String.prototype.split()` but where the limit parameter causes the
  * remainder of the string to be grouped together in a final entry.
- * 
+ *
  * @private
  * @param {string} string
  * @param {string} separator

524178b854ba01433b3d733691b95fba0ed60741
Emanuel Rabina <emanuelrabina@gmail.com>
2026-01-28 09:39:41 +1300

    Node includes all Intl and locale data now, so shouldn't need polyfills

--- vitest.setup.js
+++
@@ -1,5 +0,0 @@
-// Node has an Intl object, but doesn't ship with any locale information, so we
-// need to patch parts of it to work as it does in the browser.
-import '@formatjs/intl-locale/polyfill';
-import '@formatjs/intl-numberformat/polyfill';
-import '@formatjs/intl-numberformat/locale-data/en-NZ';
